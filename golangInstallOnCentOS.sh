556c3804d0fe14e568aaf33029b2fc0a45a888da
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-02-24 16:49:34 +0800

    golang install on CentOS

---
+++ golangInstallOnCentOS.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -L https://storage.googleapis.com/golang/go1.7.1.linux-amd64.tar.gz | tar -C /usr/local -zxf -
+echo 'export GOPATH=/gopath/' >> /root/.bashrc
+echo 'export PATH=$PATH:$GOPATH/bin:/usr/local/bin:/usr/local/go/bin/' >> /root/.bashrc
+source /root/.bashrc
+go get github.com/tools/godep
+
