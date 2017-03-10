d0b5228b9740e7d0068db65735e4148c8e1b317a
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-03-10 15:46:09 +0800

    ovs and ovn install from sources on ubuntu

---
+++ OvsAndOvnInstallOnUbuntu.sh
@@ -0,0 +1,42 @@
+#/bin/sh
+
+# Install a few pre-requisties packages
+apt-get update
+apt-get install -y build-essential fakeroot debhelper \
+                    autoconf automake bzip2 libssl-dev \
+                    openssl graphviz python-all procps \
+                    python-dev python-setuptools \
+                    python-twisted-conch libtool git dh-autoreconf \
+                    linux-headers-$(uname -r)
+easy_install -U pip
+
+OVS=$HOME/ovs
+
+# Clone the OVS repo
+git clone https://github.com/openvswitch/ovs.git $OVS
+cd $OVS
+
+# Configure and compile the sources
+./boot.sh
+./configure --prefix=/usr --localstatedir=/var --sysconfdir=/etc --enable-ssl --with-linux=/lib/modules/`uname -r`/build
+make -j3
+
+# Install the executables
+make install
+make modules_install
+
+# Install OVS python libraries
+pip install ovs
+
+# Create a depmod.d file to use OVS kernel modules
+# from this repo instead of upstream linux
+cat > /etc/depmod.d/openvswitch.conf << EOF
+override openvswitch * extra
+override vport-* * extra
+EOF
+
+# Copy a startup script and start OVS
+depmod -a
+cp debian/openvswitch-switch.init /etc/init.d/openvswitch-switch
+/etc/init.d/openvswitch-switch force-reload-kmod
+
