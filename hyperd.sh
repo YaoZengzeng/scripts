741b5e1e2d9dc084e9d13e744f26ee59b130b112
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-03-27 16:02:11 +0800

    install essential tools in clean OS

--- hyperd.sh
+++ hyperd.sh
@@ -1,4 +1,23 @@
-#!/bin/bash
+#!/bin/bash\
+set -o errexit
+
+#install essential tools
+yum install -y git cmake gcc g++ autoconf automake device-mapper-devel sqlite-devel pcre-devel libsepol-devel libselinux-devel systemd-container-devel automake autoconf gcc make glibc-devel glibc-devel.i686 libvirt-devel
+
+yum install -y libvirt
+
+yum install -y qemu.x86_64
+
+yum install -y qemu-kvm.x86_64
+
+cat >> /etc/libvirt/qemu.conf <<EOF
+user = "root"
+group = "root"
+clear_emulator_capabilities = 0
+EOF
+
+systemctl restart libvirtd
+systemctl enable libvirtd
 
 HYPERHQ_ROOT=$GOPATH/src/github.com/hyperhq
 
@@ -21,9 +40,15 @@ else
 	cd $HYPERHQ_ROOT/hyperd
 	./autogen.sh && ./configure --prefix=/usr && make && make install
 
-	cat >/etc/hyper/config <<EOF
+	mkdir /etc/hyper
+	touch /etc/hyper/config
+
+	cat > /etc/hyper/config <<EOF
 Kernel=/var/lib/hyper/kernel
 Initrd=/var/lib/hyper/hyper-initrd.img
+DisableIptables=true
+StorageDriver=devicemapper
+Hypervisor=libvirt
 gRPCHost=127.0.0.1:22318
 EOF
 
@@ -36,6 +61,4 @@ else
 	cd $HYPERHQ_ROOT/hyperstart
 	./autogen.sh && ./configure && make && /bin/cp build/hyper-initrd.img build/kernel /var/lib/hyper/
 fi
-
-((hyperd --v=3 2>&1 | tee $HOME/log/hyperd.log)&)
 
