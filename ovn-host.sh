51fa010683fa522e387a07072f3ccf2446733d1e
YaoZengzeng <yaozengzeng@zju.edu.cn>
2017-09-21 10:44:53 +0800

    update ovn install script to version 2.7.0-1

--- ovn-host.sh
+++ ovn-host.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-sudo apt-get install openvswitch-common openvswitch-switch
+# Add external repos to install docker and OVS from packages.
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+echo "deb https://packages.wand.net.nz $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/wand.list
+sudo curl https://packages.wand.net.nz/keyring.gpg -o /etc/apt/trusted.gpg.d/wand.gpg
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo su -c "echo \"deb https://apt.dockerproject.org/repo ubuntu-xenial main\" >> /etc/apt/sources.list.d/docker.list"
+sudo apt-get update
 
-sudo apt-get install ovn-common ovn-host
+# Install OVS and dependencies
+sudo apt-get build-dep dkms
+sudo apt-get install python-six openssl python-pip -y
+sudo -H pip install --upgrade pip
+
+sudo apt-get install openvswitch-datapath-dkms=2.7.0-1 -y
+sudo apt-get install openvswitch-switch=2.7.0-1 openvswitch-common=2.7.0-1 -y
+sudo -H pip install ovs
+
+sudo apt-get install  ovn-common=2.7.0-1 ovn-host=2.7.0-1 -y
 
