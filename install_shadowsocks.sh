154c18c86303221919a824fb1afd56a3bad31b5a
Yao Zengzeng <yaozengzeng@huawei.com>
2023-04-22 20:52:57 +0800

    Update and rename shadowsocksServerSetup.sh to install_shadowsocks.sh

---
+++ install_shadowsocks.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.3/shadowsocks-v1.15.3.x86_64-unknown-linux-gnu.tar.xz
+
+https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.3/shadowsocks-v1.15.3.x86_64-unknown-linux-gnu.tar.xz
--- shadowsocksServerSetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-yum install -y python-setuptools && easy_install pip
-pip install shadowsocks
-
-ssserver -p 443 -k password -m rc4-md5 -d start
-
