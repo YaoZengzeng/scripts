e4186bf86b0639fc8daa67da0d0033f51058d237
Yao Zengzeng <yaozengzeng@huawei.com>
2023-04-22 21:25:37 +0800

    Update install_shadowsocks.sh

--- install_shadowsocks.sh
+++ install_shadowsocks.sh
@@ -2,4 +2,7 @@
 
 wget https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.3/shadowsocks-v1.15.3.x86_64-unknown-linux-gnu.tar.xz
 
-https://github.com/shadowsocks/shadowsocks-rust/releases/download/v1.15.3/shadowsocks-v1.15.3.x86_64-unknown-linux-gnu.tar.xz
+tar -xvJf shadowsocks-v1.15.3.x86_64-unknown-linux-gnu.tar.xz
+
+cp sslocal /usr/local/bin
+cp ssserver /usr/local/bin
