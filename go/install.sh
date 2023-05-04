2eadef62cca4c10b1bbad925e7b34a96e5846a8e
YaoZengzeng <yaozengzeng@huawei.com>
2023-05-04 16:19:31 +0800

    update go/install.sh

--- go/install.sh
+++ go/install.sh
@@ -4,4 +4,7 @@ wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
 
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
 
-# add 'export PATH=$PATH:/usr/local/go/bin' to /root/.bashrc
+rm go1.20.3.linux-amd64.tar.gz
+
+echo "export PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> /root/.bashrc
+
