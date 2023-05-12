d159c65670d35374c04222ab0674adaea72bc138
YaoZengzeng <yaozengzeng@huawei.com>
2023-05-12 11:18:20 +0800

    allow specifying golang version

--- go/install.sh
+++ go/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-wget https://go.dev/dl/go1.20.3.linux-amd64.tar.gz
+VERSION=1.19.3
 
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.3.linux-amd64.tar.gz
+wget https://go.dev/dl/go$VERSION.linux-amd64.tar.gz
 
-rm go1.20.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go$VERSION.linux-amd64.tar.gz
+
+rm go$VERSION.linux-amd64.tar.gz
 
 echo "export PATH=$PATH:/usr/local/go/bin:/root/go/bin" >> /root/.bashrc
 
