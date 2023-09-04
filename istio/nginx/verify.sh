1bd6bdbfa6496376b1dd4807d791ee44f8d0e0a9
YaoZengzeng <yaozengzeng@huawei.com>
2023-09-04 08:37:14 +0000

    kubectl install

--- istio/nginx/verify.sh
+++ istio/nginx/verify.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-NGINX_HOST="10.102.72.156"
-
-kubectl exec -n sleep deploy/sleep -- curl -s -v http://$NGINX_HOST
+kubectl exec  deploy/sleep -- curl -s -v http://nginx.default
---
+++ k8s/install_kubectl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install latest version
+# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+# install specific version
+curl -LO https://dl.k8s.io/release/v1.26.3/bin/linux/amd64/kubectl
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+kubectl version --client
