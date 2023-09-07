8be47ddda550cd5478b4f2391f6a6ba92c300c35
Yao Zengzeng <yaozengzeng@huawei.com>
2023-09-07 17:05:40 +0800

    Update k8s download.sh

--- k8s/download.sh
+++ k8s/download.sh
@@ -4,9 +4,9 @@ sudo apt-get update
 
 sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+curl -fsSL  https://packages.cloud.google.com/apt/doc/apt-key.gpg|sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/k8s.gpg
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # 下载指定版本的k8s组件
 # apt-cache madison kubectl | grep 1.21 // 查看支持哪些k8s版本
