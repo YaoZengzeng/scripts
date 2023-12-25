9814d7955583c3eb34daa68316dbe0e5e697db9a
YaoZengzeng <yaozengzeng@huawei.com>
2023-12-25 11:33:05 +0000

    optimize websocket client-load

--- istio/websocket/client-load.go
+++ istio/websocket/client-load.go
@@ -10,6 +10,7 @@ package main
 import (
 	"flag"
 	"log"
+	"net/http"
 	"net/url"
 	"os"
 	"os/signal"
@@ -20,11 +21,117 @@ import (
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
+var listenAddr = flag.String("listenAddr", "localhost:8090", "listener address for http service")
 var c = flag.Int("c", 1, "client connection number, default value is 1")
 var t = flag.Duration("t", 10*time.Second, "client connection duration, default value is 10s")
 var l = flag.Bool("l", false, "enable log output to stdout")
-var r = flag.Bool("r", false, "rotate connections")
-var i = flag.Duration("i", 0*time.Second, "time interval between connection send message")
+var i = flag.Duration("i", time.Second, "time interval between connection send message")
+var r = flag.Bool("r", false, "enable connection rotate")
+
+func launchClientConnection(u string, interrupt <-chan os.Signal, rotateCh <-chan int, d time.Duration) {
+	c, _, err := websocket.DefaultDialer.Dial(u, nil)
+	if err != nil {
+		log.Fatal("dial:", err)
+	}
+	defer c.Close()
+
+	done := make(chan struct{})
+	trigger := make(chan struct{})
+
+	go func() {
+		defer close(done)
+		for {
+			_, message, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				return
+			}
+			if *l {
+				log.Printf("CLIENT RECV: %s", message)
+			}
+			trigger <- struct{}{}
+		}
+	}()
+
+	ticker := time.NewTicker(d)
+
+	cleanup := func() {
+		// Cleanly close the connection by sending a close message and then
+		// waiting (with timeout) for the server to close the connection.
+		err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
+		if err != nil {
+			log.Println("write close:", err)
+			return
+		}
+		if *l {
+			log.Printf("clean up connection")
+		}
+
+		select {
+		case <-done:
+		case <-time.After(time.Second):
+		}
+	}
+
+	for {
+		if *l {
+			log.Printf("CLIENT WRITE MESSAGE")
+		}
+		err := c.WriteMessage(websocket.TextMessage, []byte("write message from client"))
+		if err != nil {
+			log.Println("write:", err)
+			return
+		}
+		select {
+		case <-done:
+			return
+		case <-ticker.C:
+			log.Printf("Triggered! DURATION is %v", d)
+			ticker.Stop()
+			cleanup()
+			return
+		case <-interrupt:
+			log.Println("interrupt")
+			cleanup()
+			return
+		case id := <-rotateCh:
+			log.Printf("ROTATE! WORKER ID IS %v", id)
+			cleanup()
+			return
+		case <-trigger:
+			if *i != 0 {
+				time.Sleep(*i)
+			}
+		}
+	}
+}
+
+type worker struct {
+	rotateCh chan int
+}
+
+type client struct {
+	workers []*worker
+
+	lock sync.Mutex
+}
+
+func (cli *client) rotate(w http.ResponseWriter, r *http.Request) {
+	if cli.lock.TryLock() {
+		defer cli.lock.Unlock()
+		// Default interval is 1s per connection.
+		interval := time.Second
+		p := r.URL.Query().Get("interval")
+		if i, err := time.ParseDuration(p); err == nil {
+			interval = i
+		}
+		log.Printf("***START ROTATE, INTERVAL IS %v****", interval)
+		for i := 0; i <= *c; i++ {
+			time.Sleep(interval)
+			cli.workers[i].rotateCh <- i
+		}
+	}
+}
 
 func main() {
 	flag.Parse()
@@ -36,92 +143,29 @@ func main() {
 	u := url.URL{Scheme: "ws", Host: *addr, Path: "/echo"}
 	log.Printf("connecting to %s", u.String())
 
-	launchClientConnection := func(d time.Duration) {
-		c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-		if err != nil {
-			log.Fatal("dial:", err)
-		}
-		defer c.Close()
-
-		done := make(chan struct{})
-		trigger := make(chan struct{})
-
-		go func() {
-			defer close(done)
-			for {
-				_, message, err := c.ReadMessage()
-				if err != nil {
-					log.Println("read:", err)
-					return
-				}
-				if *l {
-					log.Printf("CLIENT RECV: %s", message)
-				}
-				trigger <- struct{}{}
-			}
-		}()
-
-		ticker := time.NewTicker(d)
-
-		cleanup := func() {
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, ""))
-			if err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			log.Printf("clean up connection")
-			select {
-			case <-done:
-			case <-time.After(time.Second):
-			}
-		}
-
-		for {
-			if *l {
-				log.Printf("CLIENT WRITE MESSAGE")
-			}
-			err := c.WriteMessage(websocket.TextMessage, []byte("write message from client"))
-			if err != nil {
-				log.Println("write:", err)
-				return
-			}
-			select {
-			case <-done:
-				return
-			case <-ticker.C:
-				log.Printf("Triggered! DURATION is %v", d)
-				ticker.Stop()
-				cleanup()
-				return
-			case <-interrupt:
-				log.Println("interrupt")
-				cleanup()
-				return
-			case <-trigger:
-				if *i != 0 {
-					time.Sleep(*i)
-				}
-			}
-		}
-	}
+	cli := &client{}
 
 	var wg sync.WaitGroup
-	for i := 1; i <= *c; i++ {
+	for i := 0; i <= *c; i++ {
 		wg.Add(1)
-		go func(i int) {
+		rotateCh := make(chan int)
+		cli.workers = append(cli.workers, &worker{rotateCh: rotateCh})
+		go func() {
 			defer wg.Done()
 			if *r {
-				log.Printf("***ROTATE CONNECTIONS!***")
-				launchClientConnection(time.Duration(i) * time.Second)
 				for {
-					launchClientConnection(*t)
+					launchClientConnection(u.String(), interrupt, rotateCh, *t)
 				}
 			} else {
-				launchClientConnection(*t)
+				launchClientConnection(u.String(), interrupt, rotateCh, *t)
 			}
-		}(i)
+		}()
+	}
+
+	if *r {
+		http.HandleFunc("/rotate", cli.rotate)
+
+		go http.ListenAndServe(*listenAddr, nil)
 	}
 
 	wg.Wait()
