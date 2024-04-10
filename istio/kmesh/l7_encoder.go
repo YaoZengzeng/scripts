4654518be101d9f236847de4c1e9f303d4b249c0
YaoZengzeng <yaozengzeng@huawei.com>
2024-04-10 18:47:27 +0800

    update 2024.4.10

--- istio/kmesh/l7_encoder.go
+++ istio/kmesh/l7_encoder.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -23,15 +25,51 @@ func main() {
 	tlvTypeService := 0x1
 	tlvTypeEnding := 0xfe
 
-	msg := []byte{byte(tlvTypeService), byte(len(*serviceAddr))}
-	msg = append(msg, []byte(*serviceAddr)...)
+	ip, portString, err := net.SplitHostPort(*serviceAddr)
+	if err != nil {
+		fmt.Println("invalid ip address and port:", err)
+		os.Exit(1)
+	}
+
+	p, err := strconv.ParseUint(portString, 10, 16)
+	if err != nil {
+		fmt.Println("failed to parse port:", err)
+		os.Exit(1)
+	}
+	var port uint16
+	port = uint16(p)
+
+	ipParsed := net.ParseIP(ip)
+	if ipParsed == nil {
+		fmt.Println("failed to parse ip address:", err)
+		os.Exit(1)
+	}
+
+	ipBytes := ipParsed.To4()
+
+	portBytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(portBytes, port)
+
+	lengthBytes := make([]byte, 4)
+	var length uint32
+	length = uint32(len(ipBytes) + len(portBytes))
+	binary.BigEndian.PutUint32(lengthBytes, length)
+
+	fmt.Printf("lengthBytes is %v\nipBytes is %v\nportBytes is %v\n", lengthBytes, []byte(ipBytes), portBytes)
+
+	msg := []byte{byte(tlvTypeService)}
+	msg = append(msg, lengthBytes...)
+	msg = append(msg, ipBytes...)
+	msg = append(msg, portBytes...)
+
+	fmt.Printf("msg is %v\n", msg)
 	_, err = conn.Write(msg)
 	if err != nil {
 		fmt.Println("send tlv type service message failed:", err)
 		os.Exit(1)
 	}
 
-	msg = []byte{byte(tlvTypeEnding), 0x0}
+	msg = []byte{byte(tlvTypeEnding), 0x0, 0x0, 0x0, 0x0}
 	req := "GET / HTTP/1.1\r\n" + "Host: nginx.default\r\n" + "Connection: close\r\n" + "\r\n"
 
 	msg = append(msg, []byte(req)...)
