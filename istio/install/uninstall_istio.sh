6fcef2cc2a409a8ca47af312566877061407b963
YaoZengzeng <yaozengzeng@huawei.com>
2024-05-30 06:58:34 +0000

    imporve istio uninstall

--- istio/install/uninstall_istio.sh
+++ istio/install/uninstall_istio.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+kubectl label namespace default istio.io/dataplane-mode-
+kubectl label namespace default istio.io/use-waypoint-
+
+istioctl x waypoint delete --all
+
 istioctl uninstall -y --purge
+
+kubectl delete namespace istio-system
