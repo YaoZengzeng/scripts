f86bb93b0c746fcf1066f8cd5fe450dae0eb569c
YaoZengzeng <yaozengzeng@huawei.com>
2024-07-09 09:04:10 +0000

    enhancement

--- istio/install/install_ambient.sh
+++ istio/install/install_ambient.sh
@@ -4,5 +4,5 @@ kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
   { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd/experimental?ref=v1.1.0" | kubectl apply -f -; }
 
 
-istioctl install --set profile=ambient --skip-confirmation #--set components.ingressGateways[0].enabled=true --set components.ingressGateways[0].name=istio-ingressgateway --set values.cni.ambient.redirectMode="ebpf"
+istioctl install --set profile=ambient --skip-confirmation --set meshConfig.accessLogFile="/dev/stdout" #--set components.ingressGateways[0].enabled=true --set components.ingressGateways[0].name=istio-ingressgateway --set values.cni.ambient.redirectMode="ebpf"
 
