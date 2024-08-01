120882fc99a47915dfbfd39ee60722caa64f6c53
YaoZengzeng <yaozengzeng@huawei.com>
2024-08-01 20:33:36 +0800

    kmesh alias

--- istio/bookinfo/continuous-flow.sh
+++ istio/bookinfo/continuous-flow.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-kubectl exec deploy/sleep -- sh -c "while true; do curl -s http://productpage:9080/productpage | grep reviews-v.-; done"
+kubectl exec deploy/sleep -- sh -c "while true; do curl -s http://productpage:9080/productpage | grep reviews-v.-; sleep 1; done"
