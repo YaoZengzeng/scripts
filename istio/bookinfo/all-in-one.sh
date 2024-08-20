f83618f0a7be12ee6b1cb421fa3ce43e374aba25
YaoZengzeng <yaozengzeng@huawei.com>
2024-08-20 16:18:18 +0800

    add default destination rules for bookinfo

--- istio/bookinfo/all-in-one.sh
+++ istio/bookinfo/all-in-one.sh
@@ -4,5 +4,8 @@ NAMESPACE="${1:-default}"
 
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/bookinfo/platform/kube/bookinfo.yaml -n "$NAMESPACE"
 
+# Create default destination rules for all Bookinfo services
+kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.23/samples/bookinfo/networking/destination-rule-all.yaml -n "$NAMESPACE"
+
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.21/samples/sleep/sleep.yaml -n "$NAMESPACE"
 
