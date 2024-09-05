fa8b24dd956860a39a8682f306e167af2efae64a
YaoZengzeng <yaozengzeng@huawei.com>
2024-09-05 06:29:40 +0000

    update kmesh install

--- kmesh/install.sh
+++ kmesh/install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-helm install kmesh /root/kmesh/deploy/helm -n kmesh-system --create-namespace
+local_image=true
 
+if "$local_image"; then
+	helm install kmesh /root/kmesh/deploy/charts/kmesh-helm -n kmesh-system --create-namespace --set deploy.kmesh.image.repository=localhost:5000/kmesh
+else
+	helm install kmesh /root/kmesh/deploy/charts/kmesh-helm -n kmesh-system --create-namespace
+fi
