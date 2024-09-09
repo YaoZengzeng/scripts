c533658e1637384ce4b42ac2369549112a31349a
YaoZengzeng <yaozengzeng@huawei.com>
2024-09-09 03:25:19 +0000

    include eds in envoy config dump

--- envoy/configdump.sh
+++ envoy/configdump.sh
@@ -3,7 +3,7 @@
 NAMESPACE="${2:-default}"
 
 if [ "$IPV6" = "true" ]; then
-	kubectl exec "$1" -n "$NAMESPACE" -- curl [::]:15000/config_dump > envoy.dump
+	kubectl exec "$1" -n "$NAMESPACE" -- curl [::]:15000/config_dump?include_eds > envoy.dump
 else
-	kubectl exec "$1" -n "$NAMESPACE" -- curl 127.0.0.1:15000/config_dump > envoy.dump
+	kubectl exec "$1" -n "$NAMESPACE" -- curl 127.0.0.1:15000/config_dump?include_eds > envoy.dump
 fi
--- envoy/sidecar_configdump.sh
+++ envoy/sidecar_configdump.sh
@@ -2,4 +2,4 @@
 
 NAMESPACE="${2:-default}"
 
-kubectl exec "$1" -c istio-proxy -n "$NAMESPACE" -- curl 127.0.0.1:15000/config_dump > envoy_sidecar.dump
+kubectl exec "$1" -c istio-proxy -n "$NAMESPACE" -- curl 127.0.0.1:15000/config_dump?include_eds > envoy_sidecar.dump
