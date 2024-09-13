0b3d5e62d86c62b88acb53590e826eb1987b2dd5
YaoZengzeng <yaozengzeng@huawei.com>
2024-09-13 03:15:14 +0000

    overwrite dp label

--- istio/manage/add_dp_label.sh
+++ istio/manage/add_dp_label.sh
@@ -3,6 +3,6 @@
 NAMESPACE="${1:-default}"
 MODE="${2:-Kmesh}"
 
-kubectl label namespace "$NAMESPACE" istio.io/dataplane-mode=$MODE
+kubectl label namespace "$NAMESPACE" istio.io/dataplane-mode=$MODE --overwrite
 
 kubectl get namespace -L istio.io/dataplane-mode
--- istio/mirror/verify.sh
+++ istio/mirror/verify.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-kubectl exec deploy/sleep -c sleep -- curl -sS http://httpbin:8000/headers
+NAMESPACE="${1:-default}"
+
+kubectl exec -n $NAMESPACE deploy/sleep -c sleep -- curl -sS http://httpbin:8000/headers
