5fec4e41a90bfb483011919944bf2495b329ec65
YaoZengzeng <yaozengzeng@huawei.com>
2024-10-28 07:22:29 +0000

    add missing kmeshctl replacement

--- istio/bookinfo/all-in-one-in-kmesh.sh
+++ istio/bookinfo/all-in-one-in-kmesh.sh
@@ -6,6 +6,6 @@ kubectl label namespace "$NAMESPACE" istio.io/dataplane-mode=Kmesh
 
 bash all-in-one.sh "$NAMESPACE"
 
-istioctl waypoint apply -n "$NAMESPACE" --name reviews-svc-waypoint --image "ghcr.io/yaozengzeng/waypoint:latest"
+kmeshctl waypoint apply -n "$NAMESPACE" --name reviews-svc-waypoint --image "ghcr.io/yaozengzeng/waypoint:latest"
 
 kubectl label service reviews -n "$NAMESPACE" istio.io/use-waypoint=reviews-svc-waypoint
