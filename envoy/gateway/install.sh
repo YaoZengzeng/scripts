d2290d110d5157ac9956c9ab696183335bdd1c6c
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-01 09:11:57 +0000

    update envoy gateway install

--- envoy/gateway/install.sh
+++ envoy/gateway/install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-kubectl apply -f https://github.com/envoyproxy/gateway/releases/download/latest/install.yaml
+helm install eg oci://docker.io/envoyproxy/gateway-helm --version v1.1.2 -n envoy-gateway-system --create-namespace
+
+kubectl wait --timeout=5m -n envoy-gateway-system deployment/envoy-gateway --for=condition=Available
+
