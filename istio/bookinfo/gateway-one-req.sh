2c776af192ff5adf077068d14c8da48374e47964
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-05 07:39:11 +0000

    add gateway access for bookinfo

---
+++ istio/bookinfo/gateway-one-req.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export INGRESS_NAME=istio-ingressgateway
+export INGRESS_NS=istio-system
+
+export INGRESS_HOST=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+export SECURE_INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+export TCP_INGRESS_PORT=$(kubectl -n "$INGRESS_NS" get service "$INGRESS_NAME" -o jsonpath='{.spec.ports[?(@.name=="tcp")].port}')
+
+export GATEWAY_URL=$INGRESS_HOST:$INGRESS_PORT
+
+curl -s "http://${GATEWAY_URL}/productpage" | grep -o "<title>.*</title>"
