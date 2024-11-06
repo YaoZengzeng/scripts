3428d40fe9d422444e68591afd1d3ba7d5476d9b
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-06 09:25:51 +0000

    cleanup bookinfo

--- istio/bookinfo/one-req.sh
+++ istio/bookinfo/one-req.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# install sleep first
+
 NAMESPACE="${1:-default}"
 
 kubectl exec deploy/sleep -n "$NAMESPACE" -- sh -c "curl -s http://productpage:9080/productpage | grep reviews-v.-"
--- istio/bookinfo/verify.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# install sleep first
-
-kubectl exec deploy/sleep -- curl -s http://productpage:9080/productpage  | grep reviews-v.-
-
