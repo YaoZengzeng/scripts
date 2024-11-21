18e0bc68997bb0b1f7a15bc216a7f6aad0c902c6
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-21 06:21:20 +0000

    fix kmesh all in one scripts

--- kmesh/all_in_one.sh
+++ kmesh/all_in_one.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-bash /root/scripts/k8s/kind/create_kind_cluster.sh
+CLUSTER_NAME="${1:-kmesh}"
+
+bash /root/scripts/k8s/kind/create_kind_cluster.sh $CLUSTER_NAME
 
 bash /root/scripts/istio/install/install_ambient.sh
+
+export local_image=false 
 
 bash ./install.sh
 
--- kmesh/install.sh
+++ kmesh/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-local_image=true
+local_image="${local_image:-true}"
 
 if "$local_image"; then
 	helm install kmesh /root/kmesh/deploy/charts/kmesh-helm -n kmesh-system --create-namespace --set deploy.kmesh.image.repository=localhost:5000/kmesh
