f8f7abdc8aaa0fb367ff80a52610bb4e7f95ac2d
YaoZengzeng <yaozengzeng@huawei.com>
2024-11-28 07:23:41 +0000

    seperate model installation and verification

---
+++ llm/ollama/gateway/install_model.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ref: https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
+
+HOST="${HOST:-localhost:11434}"
+
+# Pull model first
+curl --header "Host: www.ollama.com" http://$HOST/api/pull -d '{
+  "name": "llama3.2"
+}'
+
+# List local models
+curl --header "Host: www.ollama.com" http://$HOST/api/tags
--- llm/ollama/gateway/verify.sh
+++ llm/ollama/gateway/verify.sh
@@ -4,16 +4,8 @@
 
 HOST="${HOST:-localhost:11434}"
 
-# Pull model first
-curl --header "Host: www.ollama.com" http://$HOST/api/pull -d '{
-  "name": "llama3.2"
-}'
-
-# List local models
-curl --header "Host: www.ollama.com" http://$HOST/api/tags
-
 # Generate a completion
-curl --header "Host: www.ollama.com" http://$HOST/api/generate -d '{
+curl -v --header "Host: www.ollama.com" http://$HOST/api/generate -d '{
   "model": "llama3.2",
   "prompt": "Who are you?"
 }'
