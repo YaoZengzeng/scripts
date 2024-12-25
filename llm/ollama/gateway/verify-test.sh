1852a4b35d21112a5b877596aaed8bd34c97c496
YaoZengzeng <yaozengzeng@huawei.com>
2024-12-25 08:51:15 +0000

    add ollama test verify

---
+++ llm/ollama/gateway/verify-test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ref: https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
+
+HOST="${HOST:-localhost:11434}"
+
+# Generate a completion
+curl -v --header "Host: www.ollama-test.com" http://$HOST/api/generate -d '{
+  "model": "llama3.2",
+  "prompt": "Who are you?"
+}'
