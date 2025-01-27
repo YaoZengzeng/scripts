a9053b51d36bd806e1efbafe09ee2a8f97fece44
YaoZengzeng <yaozengzeng@huawei.com>
2025-01-27 09:01:12 +0000

    optimize ollama verify

--- llm/ollama/forward/forward.sh
+++ llm/ollama/forward/forward.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+NAMESPACE="${1:-ollama}"
+
 LOCAL_PORT=11434
 
-export POD_NAME=$(kubectl get pods --namespace ollama -l "app.kubernetes.io/name=ollama,app.kubernetes.io/instance=ollama" -o jsonpath="{.items[0].metadata.name}")
+export POD_NAME=$(kubectl get pods --namespace $NAMESPACE -l "app.kubernetes.io/name=ollama,app.kubernetes.io/instance=ollama" -o jsonpath="{.items[0].metadata.name}")
 
-export CONTAINER_PORT=$(kubectl get pod --namespace ollama $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
+export CONTAINER_PORT=$(kubectl get pod --namespace $NAMESPACE $POD_NAME -o jsonpath="{.spec.containers[0].ports[0].containerPort}")
 
 echo "Visit http://127.0.0.1:$LOCAL_PORT to use your application"
 
-kubectl --namespace ollama port-forward $POD_NAME $LOCAL_PORT:$CONTAINER_PORT
+kubectl --namespace $NAMESPACE port-forward $POD_NAME $LOCAL_PORT:$CONTAINER_PORT
--- llm/ollama/gateway/verify-vm.sh
+++ llm/ollama/gateway/verify-vm.sh
@@ -2,10 +2,18 @@
 
 # ref: https://github.com/ollama/ollama/blob/main/docs/api.md#generate-a-completion
 
+loop_count=1
+
+if [ $# -ge 1 ]; then
+  loop_count=$1
+fi
+
 HOST="${HOST:-localhost:11434}"
 
-# Generate a completion
-curl -v --header "Host: www.ollama.com" http://$HOST/api/generate -d '{
-  "model": "llama",
-  "prompt": "Who are you?"
-}'
+for (( i=1; i<=loop_count; i++ )); do
+  # Generate a completion
+  curl -v --header "Host: www.ollama.com" http://$HOST/api/generate -d '{
+    "model": "llama",
+    "prompt": "Who are you?"
+  }'
+done
