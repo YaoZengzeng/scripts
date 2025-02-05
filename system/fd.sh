d4a97310c724925d582ca50534791991b948d067
YaoZengzeng <yaozengzeng@huawei.com>
2025-02-05 03:04:54 +0000

    merge fd.sh

--- system/fd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sysctl -w fs.inotify.max_user_watches=10000000
-sysctl -w fs.inotify.max_user_instances=10000000
-
-# To make the changes persistent, edit the file `/etc/sysctl.conf` and add these lines above.
--- system/persist_fd.sh
+++ system/persist_fd.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# temporary modifications
+
+# sysctl -w fs.inotify.max_user_watches=10000000
+# sysctl -w fs.inotify.max_user_instances=10000000
+
+# make the changes persistent
+
 echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
 echo "fs.inotify.max_user_instances = 512" >> /etc/sysctl.conf
