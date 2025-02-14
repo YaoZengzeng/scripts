92302db431d0208b615be13b12c39199f6ff83c6
YaoZengzeng <yaozengzeng@huawei.com>
2025-02-14 03:22:16 +0000

    optimize vllm run

--- llm/vllm/verify/run_model.sh
+++ llm/vllm/verify/run_model.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --env "HUGGING_FACE_HUB_TOKEN=$1" -p 8000:8000 --ipc=host ghcr.io/yaozengzeng/vllm-cpu-env --model mistralai/Mistral-7B-v0.1
+TOKEN=xxxx
+
+docker run --env "HUGGING_FACE_HUB_TOKEN=$TOKEN" -p 8000:8000 --ipc=host ghcr.io/yaozengzeng/vllm-cpu-env --model mistralai/Mistral-7B-v0.1
--- llm/vllm/verify/verify.sh
+++ llm/vllm/verify/verify.sh
@@ -3,7 +3,7 @@
 curl http://localhost:8000/v1/completions \
     -H "Content-Type: application/json" \
     -d '{
-        "model": "facebook/opt-125m",
+        "model": "mistralai/Mistral-7B-v0.1",
         "prompt": "San Francisco is a",
         "max_tokens": 7,
         "temperature": 0
