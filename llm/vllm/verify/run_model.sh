58eaa59ec330d1a40c9e741ac7876818c20844f7
YaoZengzeng <yaozengzeng@huawei.com>
2025-02-17 06:26:26 +0000

    specify model

--- llm/vllm/verify/run_model.sh
+++ llm/vllm/verify/run_model.sh
@@ -2,4 +2,8 @@
 
 TOKEN=xxxx
 
-docker run --env "HUGGING_FACE_HUB_TOKEN=$TOKEN" -p 8000:8000 --ipc=host ghcr.io/yaozengzeng/vllm-cpu-env --model mistralai/Mistral-7B-v0.1
+MODEL="${1:-"mistralai/Mistral-7B-v0.1"}"
+
+echo "run model $MODEL"
+
+docker run --env "HUGGING_FACE_HUB_TOKEN=$TOKEN" -p 8000:8000 --ipc=host ghcr.io/yaozengzeng/vllm-cpu-env --model $MODEL
