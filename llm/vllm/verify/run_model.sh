6d147fbcc7bd804b7fb9257a5c31110f85f26be0
YaoZengzeng <yaozengzeng@huawei.com>
2025-02-17 11:59:11 +0000

    update model to deepseek R1

--- llm/vllm/verify/run_model.sh
+++ llm/vllm/verify/run_model.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TOKEN=xxxx
+TOKEN=$(cat /root/huggingface-token)
 
-MODEL="${1:-"mistralai/Mistral-7B-v0.1"}"
+MODEL="${1:-"deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"}"
 
 echo "run model $MODEL"
 
-docker run --env "HUGGING_FACE_HUB_TOKEN=$TOKEN" -p 8000:8000 --ipc=host ghcr.io/yaozengzeng/vllm-cpu-env --model $MODEL
+docker run --env "HUGGING_FACE_HUB_TOKEN=$TOKEN" -v /root/.cache/huggingface:/root/.cache/huggingface -p 8000:8000 --ipc=host ghcr.io/yaozengzeng/vllm-cpu-env --model $MODEL
--- llm/vllm/verify/verify.sh
+++ llm/vllm/verify/verify.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+MODEL="${1:-"deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"}"
+
 curl http://localhost:8000/v1/completions \
     -H "Content-Type: application/json" \
-    -d '{
-        "model": "mistralai/Mistral-7B-v0.1",
-        "prompt": "San Francisco is a",
-        "max_tokens": 7,
-        "temperature": 0
-    }'
+    -d "{
+        \"model\": \"$MODEL\",
+        \"prompt\": \"San Francisco is a\",
+        \"max_tokens\": 7,
+        \"temperature\": 0
+    }"
