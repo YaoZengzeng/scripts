7d55e0719c90aeae06effce148e9fec55b8c759a
YaoZengzeng <yaozengzeng@huawei.com>
2025-03-06 09:03:06 +0000

    enrich loop_test

--- kmesh/loop_test.sh
+++ kmesh/loop_test.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 while true; do
+
+    bash /root/scripts/k8s/delete_ns.sh "echo"
     
     cd /root/kmesh
 
     #./test/e2e/run_test.sh --skip-install-dep --skip-build --ipv6 -run 'TestKmeshRestart' --skip-cleanup-apps -count 1
-    ./test/e2e/run_test.sh --only-run-tests -run 'TestKmeshRestart' --skip-cleanup-apps -count 1
+    #./test/e2e/run_test.sh --only-run-tests -run 'TestKmeshRestart' --skip-cleanup-apps -count 1
+    #./test/e2e/run_test.sh --only-run-tests -run 'TestKmeshRestart' --skip-cleanup-apps --istio.test.ci -count 100 -failfast
+    ./test/e2e/run_test.sh --skip-install-dep --skip-build --ipv6  --skip-cleanup-apps -count 1
     
     # check the exit status of a command
     if [ $? -ne 0 ]; then
@@ -13,5 +17,4 @@ while true; do
         break
     fi
 
-    bash /root/scripts/k8s/delete_ns.sh "echo"
 done
