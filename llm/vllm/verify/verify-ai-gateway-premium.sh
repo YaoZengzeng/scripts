5aa65aa5f970f071b7b48ffd53d3db92e26813c1
YaoZengzeng <yaozengzeng@huawei.com>
2025-04-27 08:49:44 +0000

    verify ai gateway with premium header

--- llm/vllm/verify/verify-ai-gateway-premium.sh
+++ llm/vllm/verify/verify-ai-gateway-premium.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-MODEL="${1:-"deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"}"
+MODEL="${1:-"deepseek-r1"}"
 
-curl -v http://localhost:8080/v1/completions \
+HOST="127.0.0.1:8080"
+
+curl -v http://$HOST/v1/completions \
     -H "Content-Type: application/json" \
+    -H "user-type: premium" \
     -d "{
         \"model\": \"$MODEL\",
         \"prompt\": \"San Francisco is a\",
