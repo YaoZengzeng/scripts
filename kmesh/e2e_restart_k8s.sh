3b94f3f67237504c4780952badd1e6f245ceb477
YaoZengzeng <yaozengzeng@huawei.com>
2025-04-29 06:22:32 +0000

    update kmesh e2e

--- kmesh/e2e_restart_k8s.sh
+++ kmesh/e2e_restart_k8s.sh
@@ -2,4 +2,5 @@
 
 cd /root/kmesh
 
-./test/e2e/run_test.sh --skip-install-dep --skip-build -run "TestBookinfo\|TestKmeshRestart" --skip-cleanup-apps
+#./test/e2e/run_test.sh --skip-install-dep --skip-build -run "TestBookinfo\|TestKmeshRestart" --skip-cleanup-apps
+./test/e2e/run_test.sh --skip-install-dep --skip-build --skip-cleanup-apps
