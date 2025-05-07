d47711dbb1f53f2688e885d3df2288ed2c580d64
YaoZengzeng <yaozengzeng@huawei.com>
2025-05-07 07:29:12 +0000

    run e2e continuously

---
+++ kmesh/e2e_continuous_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /root/kmesh
+
+#./test/e2e/run_test.sh --only-run-tests -run "TestServiceEntrySelectsWorkloadEntry" --skip-cleanup-apps
+./test/e2e/run_test.sh --only-run-tests -count 1000
--- kmesh/e2e_restart_k8s.sh
+++ kmesh/e2e_restart_k8s.sh
@@ -3,4 +3,4 @@
 cd /root/kmesh
 
 #./test/e2e/run_test.sh --skip-install-dep --skip-build -run "TestBookinfo\|TestKmeshRestart" --skip-cleanup-apps
-./test/e2e/run_test.sh --skip-install-dep --skip-build --skip-cleanup-apps
+./test/e2e/run_test.sh --skip-install-dep --skip-build #--skip-cleanup-apps
