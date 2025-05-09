ca8279a6ad998a4e974ee494bb4ee7742b9a7f1f
YaoZengzeng <yaozengzeng@huawei.com>
2025-05-09 09:23:14 +0000

    adjust timeout for continuous kmesh test

--- kmesh/e2e_continuous_run.sh
+++ kmesh/e2e_continuous_run.sh
@@ -3,4 +3,4 @@
 cd /root/kmesh
 
 #./test/e2e/run_test.sh --only-run-tests -run "TestServiceEntrySelectsWorkloadEntry" --skip-cleanup-apps
-./test/e2e/run_test.sh --only-run-tests -count 1000
+./test/e2e/run_test.sh --only-run-tests -count 1000 -timeout 1000000s
