f8a0b510704da2de8a1e80f8510f385772362c2a
YaoZengzeng <yaozengzeng@huawei.com>
2025-05-14 02:14:11 +0000

    improve kmesh continuous e2e

--- kmesh/e2e_continuous_run.sh
+++ kmesh/e2e_continuous_run.sh
@@ -2,5 +2,5 @@
 
 cd /root/kmesh
 
-#./test/e2e/run_test.sh --only-run-tests -run "TestServiceEntrySelectsWorkloadEntry" --skip-cleanup-apps
+#./test/e2e/run_test.sh --only-run-tests -run "TestCrossNamespace" -count 1000 -timeout 1000000s #--skip-cleanup-apps
 ./test/e2e/run_test.sh --only-run-tests -count 1000 -timeout 1000000s
