da7f22e3d211be50df5a2c381accf4b116935635
YaoZengzeng <yaozengzeng@huawei.com>
2025-06-14 08:52:54 +0000

    stream for completion api

--- llm/vllm/verify/verify-ai-gateway-stream.sh
+++ llm/vllm/verify/verify-ai-gateway-stream.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-HOST="${HOST:-"127.0.0.1:80"}"
-
 MODEL="${1:-"deepseek-r1"}"
 
-curl -v http://$HOST/v1/chat/completions \
+HOST="${HOST:-127.0.0.1:80}"
+
+curl -v http://$HOST/v1/completions \
     -H "Content-Type: application/json" \
     -d "{
         \"model\": \"$MODEL\",
-	\"messages\": [{\"role\": \"user\", \"content\": \"Who are you?\"}],
+        \"prompt\": \"San Francisco is a\",
         \"temperature\": 0,
 	\"stream\": true
     }"
