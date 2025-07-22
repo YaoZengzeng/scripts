d65761ecc978a0ad53b9ca1b3d2810bd70d314b9
YaoZengzeng <yaozengzeng@huawei.com>
2025-07-22 12:35:59 +0000

    job resources

--- llm/sglang/benchmark/run.sh
+++ llm/sglang/benchmark/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --network host ghcr.io/yaozengzeng/sglang-benchmark:qwen-7b --host 127.0.0.1 --port 80 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-7B --tokenizer deepseek-ai/DeepSeek-R1-Distill-Qwen-7B --dataset-name generated-shared-prefix
+docker run -it --network host ghcr.io/yaozengzeng/sglang-benchmark:qwen-7b --host 127.0.0.1 --port 80 --model deepseek-ai/DeepSeek-R1-Distill-Qwen-7B --tokenizer deepseek-ai/DeepSeek-R1-Distill-Qwen-7B --dataset-name generated-shared-prefix --gsp-num-groups 256 --gsp-prompts-per-group 16 --gsp-system-prompt-len 256 --gsp-question-len 2048 --gsp-output-len 256 --request-rate 10 --max-concurrency 10 --max-concurrency 10
