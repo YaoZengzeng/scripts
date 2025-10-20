beedfa9fefc47ffb4c386b86ac09eac113197c4f
YaoZengzeng <yaozengzeng@huawei.com>
2025-10-20 04:11:28 +0000

    update istio build

--- istio/build/build.sh
+++ istio/build/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-VERSION="${1:-1.23.0}"
+VERSION="${1:-1.27.2}"
 
 cp /root/istio/out/linux_amd64/pilot-discovery .
 
-docker build . --no-cache -t istio/pilot:$VERSION-distroless
+docker build . --no-cache -t ghcr.io/yaozengzeng/pilot:$VERSION-distroless
