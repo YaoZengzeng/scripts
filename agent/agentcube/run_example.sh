5ac2ccc1f1f8749dcdbe1df4935717e277de6016
YaoZengzeng <yaozengzeng@huawei.com>
2025-11-17 11:55:39 +0800

    test agentcube in test ns

--- agent/agentcube/run_example.sh
+++ agent/agentcube/run_example.sh
@@ -2,7 +2,7 @@
 
 # run the example
 
-export API_TOKEN=$(kubectl create token my-app -n default --duration=24h)
+export API_TOKEN=$(kubectl create token my-app -n test --duration=24h)
 
 cd /root/agentcube/example
 
