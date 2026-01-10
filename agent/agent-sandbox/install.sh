49f7be82f9625f9aa216afbddcb50e8dc4701264
YaoZengzeng <yaozengzeng@huawei.com>
2026-01-10 15:59:56 +0800

    agent sandbox uninstall

--- agent/agent-sandbox/install.sh
+++ agent/agent-sandbox/install.sh
@@ -3,8 +3,36 @@
 # https://github.com/kubernetes-sigs/agent-sandbox/releases
 export VERSION="v0.1.0"
 
-# To install only the core components:
-kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/manifest.yaml
+ACTION="apply"
 
-# To install the extensions components:
-kubectl apply -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/extensions.yaml
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  -u, --uninstall    Uninstall agent-sandbox components"
+    echo "  -h, --help         Show this help message"
+    exit 0
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -u|--uninstall)
+            ACTION="delete"
+            shift
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+echo "Performing ${ACTION} for agent-sandbox ${VERSION}..."
+
+# To install/uninstall the core components:
+kubectl ${ACTION} -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/manifest.yaml
+
+# To install/uninstall the extensions components:
+kubectl ${ACTION} -f https://github.com/kubernetes-sigs/agent-sandbox/releases/download/${VERSION}/extensions.yaml
