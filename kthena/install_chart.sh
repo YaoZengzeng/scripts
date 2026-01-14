cda5bec95db3ad5cc7c4485bc8b2df61e57fc585
YaoZengzeng <yaozengzeng@huawei.com>
2026-01-14 17:09:08 +0800

    update kthena chart

--- kthena/install_chart.sh
+++ kthena/install_chart.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-helm install test-release /root/kthena/charts/kthena --namespace kthena-system --create-namespace
+INSTALL_CONTROLLER=false
+
+usage() {
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  -a, --all       Install both router and controller (default: only router)"
+    echo "  -h, --help      Display this help message"
+    exit 0
+}
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -a|--all)
+            INSTALL_CONTROLLER=true
+            shift
+            ;;
+        -h|--help)
+            usage
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+helm install test-release /root/kthena/charts/kthena --namespace kthena-system --create-namespace \
+    --set workload.enabled=$INSTALL_CONTROLLER
