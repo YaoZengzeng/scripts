9e7926c1b7bddd3eeefe2f26c9d62a58020af276
YaoZengzeng <yaozengzeng@huawei.com>
2026-04-11 08:30:09 +0000

    sglang pd disaggregation example

---
+++ sglang/curl.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl http://172.235.216.136:30000/v1/chat/completions \
+  -H "Content-Type: application/json" \
+  -d '{"model":"Qwen/Qwen3-0.6B","messages":[{"role":"user","content":"Hello"}]}'
