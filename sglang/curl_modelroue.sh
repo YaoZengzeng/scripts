88a4d7b0b20b0cf0eba1192873c2a210a4a91c6b
YaoZengzeng <yaozengzeng@huawei.com>
2026-04-14 07:05:06 +0000

    update modelserver

--- sglang/curl_modelroue.sh
+++ sglang/curl_modelroue.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -v http://172.233.68.31:80/v1/chat/completions \
+curl -v http://172.233.70.170:80/v1/chat/completions \
   -H "Content-Type: application/json" \
   -d '{"model":"sglang-qwen-06b","messages":[{"role":"user","content":"Hello"}]}'
