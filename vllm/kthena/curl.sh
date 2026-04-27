e0b540ad718dcfdec71e5ef419d7dcc89ca4a8c8
YaoZengzeng <yaozengzeng@huawei.com>
2026-04-27 12:15:19 +0000

    update vllm image

--- vllm/kthena/curl.sh
+++ vllm/kthena/curl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -v http://172.236.135.114:80/v1/chat/completions \
+curl -v http://172.236.135.120:80/v1/chat/completions \
   -H "Content-Type: application/json" \
   -d '{"model":"Qwen/Qwen3-0.6B","messages":[{"role":"user","content":"Hello"}]}'
