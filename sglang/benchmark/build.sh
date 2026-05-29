d19e63ee8384792277dba1949450904dda5ab72e
YaoZengzeng <yaozengzeng@huawei.com>
2026-05-29 02:50:00 +0000

    update sglang benchmark dockerfile

--- sglang/benchmark/build.sh
+++ sglang/benchmark/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build --no-cache -t ghcr.io/yaozengzeng/benchmark:latest .
+docker build --no-cache --secret id=HF_TOKEN,env=HF_TOKEN --build-arg MODEL_ID=Qwen/Qwen3-0.6B -t ghcr.io/yaozengzeng/benchmark:qwen-3-0.6b .
