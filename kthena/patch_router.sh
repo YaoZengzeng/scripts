ac088d573c163964f7625b05436acc9d3ce208b8
YaoZengzeng <yaozengzeng@huawei.com>
2026-06-09 03:40:01 +0000

    update patch

--- kthena/patch_router.sh
+++ kthena/patch_router.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 kubectl -n kthena-system patch deployment kthena-router --type=json -p '[
-  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "ENABLE_FAIRNESS_SCHEDULING", "value": "true"}},
-  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "FAIRNESS_MAX_QPS", "value": "10"}},
-  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "FAIRNESS_MAX_CONCURRENT", "value": "0"}},
-  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "FAIRNESS_SESSION_BOOST_ENABLED", "value": "true"}},
-  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "FAIRNESS_SESSION_BOOST_TTL", "value": "60s"}},
-  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "FAIRNESS_INFLIGHT_PER_POD", "value": "8"}},
+  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "ENABLE_SESSION_BOOST", "value": "true"}},
+  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "SESSION_BOOST_HEADER", "value": "X-Correlation-ID"}},
+  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "SESSION_BOOST_TTL", "value": "60s"}},
+  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "SESSION_BOOST_GRACE_PERIOD", "value": "50ms"}},
+  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "SESSION_BOOST_POLL_INTERVAL", "value": "100ms"}},
+  {"op": "add", "path": "/spec/template/spec/containers/0/env/-", "value": {"name": "SESSION_BOOST_INFLIGHT_PER_POD", "value": "8"}},
   {"op": "replace", "path": "/spec/template/spec/containers/0/image", "value": "ghcr.io/yaozengzeng/kthena-router:latest"},
   {"op": "replace", "path": "/spec/template/spec/containers/0/imagePullPolicy", "value": "Always"}
 ]'
